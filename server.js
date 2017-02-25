ab41a356a010d4408705f911937754a5fe376f61
Mmole <blueprog42@gmail.com>
2017-02-25 17:25:31 +0100

    add api sens sms

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+var request = require('request');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
@@ -10,10 +10,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 var userList = [];
+
+/* Start Mail */
+
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: 'HecSpred@gmail.com',
+		user: '',
 		pass: ''
 	}
 });
@@ -25,7 +28,6 @@ var mailOptions = {
     text: 'Hello world ?'
 };
 
-// send mail with defined transport object
 function sendMail(data) {
 	transporter.sendMail(data, (error, info) => {
 	    if (error) {
@@ -34,9 +36,35 @@ function sendMail(data) {
 	    console.log('Message %s sent: %s', info.messageId, info.response);
 	});	
 }
+/* End Mail */
+
+function sendSms() {
+	request({
+		headers: {'content-type' : 'application/x-www-form-urlencoded'},
+	    url: 'http://www.smsenvoi.com/getapi/sendsms/',
+	    method: 'GET',
+	    qs: {
+	    	'email': "",
+	    	'apikey': "",
+	    	'message[content]': "autre test",
+	    	'message[senderlabel]': "Expediteur",
+	    	'message[recipients]': '+33652937461',
+	    	'message[subtype]': "LOWCOST",
+	    	'message[type]': "sms"
+	    },
+	},
+    function (error, response, body) {
+    	if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+        else
+        	console.log(error);
+    });
+}
 
 router.use(function(req, res, next) {
  	console.log(req.method, req.url);
+ 	console.log(req.body);
 	next();
 })
 
@@ -59,6 +87,8 @@ router.route('/userList')
 		}
 		res.send(userList);
 	});
+
+/* Email fonction */
 
 router.route('/sendMessage')
 	.post(function(req, res) {
